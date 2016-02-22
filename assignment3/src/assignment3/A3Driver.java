1d7a223759e404e5340ab10882a2c770df10e6fe
aifusenno1 <aifusenno1@gmail.com>
2016-02-21 18:23:06 -0600

    Completed Classes v1
    Item Class and its subclasses are completed
    The skeleton of Driver Class is written. Need to fill out the code.

--- assignment3/src/assignment3/A3Driver.java
+++ assignment3/src/assignment3/A3Driver.java
@@ -1,32 +1,105 @@
-package assignment3;
+package Assignment3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
-public class A3Driver 
-	{
+public class A3Driver {
+	// global variable shoppingCart
+	public static ArrayList<Item> shoppingCart = new ArrayList<Item>();
 
-	  public static void main(String[] args) 
-	  {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) throws FileNotFoundException 
+	{		
 		//Open file; file name specified in args (command line)
-		
-		//Parse input, take appropriate actions.
-		
-		//Stub for arraylist.
-		ArrayList<Item> shoppingCart = new ArrayList<Item>(); 
-		
-		// General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
-		Iterator<Item> i = shoppingCart.iterator();
-		while (i.hasNext()) 
-		{
-			Item temp = i.next();
-			temp.calculatePrice(); 
-			temp.printItemAttributes();
-			//This (above) works because of polymorphism: a determination is made at runtime, 
-			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
-			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
-		}		
-	  }
+		if (args.length != 1) {
+			// if not file or more than 1 file, throw the exception
+			throw new FileNotFoundException("Not Input File Found.");
+		}
+
+		try {
+			FileReader freader = new FileReader(args[0]);
+			BufferedReader reader = new BufferedReader(freader);
+
+			//Parse input, take appropriate actions.
+			for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+				parseInput(s);
+			}
+		}
+		catch (IOException e) {
+			System.err.println("Error: IO exception. Exiting...");
+			System.exit(-1);
+		}
+
+
+		//		Iterator<Item> i = shoppingCart.iterator();
+		//		while (i.hasNext()) 
+		//		{
+		//			Item temp = i.next();
+		//			temp.printItemAttributes();
+		//			//This (above) works because of polymorphism: a determination is made at runtime, 
+		//			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
+		//			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
+		//		}		
+	}
+	/**
+	 * parse the input string and branch to the specific operation
+	 * @param inputLine
+	 */
+	public static void parseInput(String inputLine) {
+		// split input String into individual commands
+		String[] inputCommands = inputLine.split("[\\s+]");
+		try {
+			switch(inputCommands[0]) {
+			case "insert": 
+				insert(inputCommands);
+				break;
+			case "search":
+				search(inputCommands);
+				break;
+			case "delete":
+				delete(inputCommands);
+				break;
+			case "update":
+				update(inputCommands);
+				break;
+			case "print":
+				print(inputCommands);
+				break;
+			default :
+				System.err.println("Invalid Input: Illegal Operation");
+				return;
+			}
+		}
+		// if there is no element in this array
+		catch(IndexOutOfBoundsException e) {
+			System.err.println("Invalid Input: Illegal Format");
+			return; //return to the main method, process next line
+		}
+	}
+
+	/*
+	 * methods to fill out
+	 */
+	public static void insert(String[] inputCommands){
+
+	}
+
+	public static void search(String[] inputCommands){
+
+	}
+
+	public static void delete(String[] inputCommands){
+
+	}
+
+	public static void update(String[] inputCommands){
+
+	}
+
+	public static void print(String[] inputCommands){
+
+	}
 
 }
--- assignment3/src/assignment3/Item.java
+++ assignment3/src/assignment3/Item.java
@@ -1,22 +1,41 @@
-package assignment3;
+package Assignment3;
 
 public class Item 
 {
-//Declare variables for this class. Think about its type: public, protected or private?
-
-// You will need a constructor (Why?). Create it here.
+	protected String name;
+	protected double salePrice;	// the sale price read from the input
+	protected int weight;
+	protected int quantity;
+	protected double priceAfterTax;
+	protected double shippingFee;
 	
-	float calculatePrice () 
-	{
-		float final_price = 0;
-		// Insert price calculation here
-		return final_price;
+	// constructor
+	public Item(String name, double salePrice, int quantity, int weight) {
+		this.name = name;
+		this.salePrice = salePrice;
+		this.quantity = quantity;
+		this.weight = weight;
 	}
 	
 
-	void printItemAttributes () 
-	{
-		//Print all applicable attributes of this class
+	public void printItemAttributes () {
+		
 	}
+	
+
+	public void updateQuantity(int quantity) {
+		this.quantity = quantity;
+		calcShippingFee();	// this will invoke the method in the subclass to which the variable refers
+		// so the shippingFee will be recalculated immediately
+	}
+	
+	
+	
+	// methods that will be defined in subclasses
+	// has to be protected because private methods cannot be overridden
+	// it's better to make Item an abstract class but not familiar enough
+	protected void calcPriceAfterTax() {}
+	protected void calcShippingFee() {}
+	
 
 }
